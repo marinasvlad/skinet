69b479f925b576f968699e10689af5e7e374403e
marinasvlad <57010643+marinasvlad@users.noreply.github.com>
2022-10-11 16:39:34 +0300

    End of section 16

--- API/Dtos/RegisterDto.cs
+++ API/Dtos/RegisterDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,13 @@ namespace API.Dtos
 {
     public class RegisterDto
     {
+        [Required]
         public string DisplayName { get; set; }
-
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
-
+        [Required]
+        [RegularExpression("(?=^.{6,10}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\\s).*$",ErrorMessage = "Password must have 1 upper case, 1 lower case, 1 number, 1 number and 1 non alpha numeric and at least 6 characters")]
         public string Password { get; set; }
     }
 }
